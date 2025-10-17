193e1a2b1524b514a80ca6070294d6ce15887649
= <=>
2025-10-17 12:32:52 +0800

    .

--- StarContest/2025/10/17/lcm.cpp
+++ StarContest/2025/10/17/lcm.cpp
@@ -14,7 +14,7 @@ int fp(ll x, int n) {
     return r;
 }
 struct node {
-    int l, r, v, tag = 1;
+    int l, r, v, tag;
 } tr[N << 2];
 #define ls p << 1
 #define rs p << 1 | 1
@@ -28,7 +28,7 @@ void build(int p, int l, int r) {
     pushup(p);
 }
 inline void pushdown(int p) {
-    if (tr[p].tag ^ 1) {
+    if (tr[p].tag != 1) {
         tr[ls].v = (ll) tr[ls].v * tr[p].tag % mod;
         tr[rs].v = (ll) tr[rs].v * tr[p].tag % mod;
         tr[ls].tag = (ll) tr[ls].tag * tr[p].tag % mod;
@@ -49,8 +49,10 @@ void update(int p, int l, int r, int v) {
     pushup(p);
 }
 
-int idas(int x) { return lower_bound(as.begin(), as.end(), x) - as.begin() + 1; }
-int valas(int x) { return as[x - 1]; }
+inline int idas(int x) { return lower_bound(as.begin(), as.end(), x) - as.begin() + 1; }
+inline int valas(int x) { return as[x - 1]; }
+inline int idbs(int x) { return lower_bound(bs.begin(), bs.end(), x) - bs.begin() + 1; }
+inline int valbs(int x) { return bs[x - 1]; }
 
 int main() {
     cin >> n;
@@ -59,8 +61,42 @@ int main() {
     as.erase(unique(as.begin(), as.end()), as.end());
     sort(bs.begin(), bs.end());
     bs.erase(unique(bs.begin(), bs.end()), bs.end());
-    for (int i = 1; i <= n; i++) {
-        ;
+    const int m = bs.size();
+    for (int i = 1; i <= n; i++) mk[a[i]].push_back(idbs(b[i]));
+    int sum1 = 0, sum2 = 0, sum3 = 0, sum4 = 0;
+    for (int i = 0; i < m; i++)
+        vl[i + 1] = fp(3, bs[i]);
+    build(1, 1, m);
+    for (int i = 0; i <= (int) as.size() || i <= 10; i++) {
+        for (int j : mk[i])
+            update(1, j, m, 2);
+        (sum1 += (ll) tr[1].v * fp(2, i) % mod) %= mod;
     }
+    // cout << ans << endl;
+    build(1, 1, m);
+    for (int i = 0; i < (int) as.size() || i < 10; i++) {
+        for (int j : mk[i])
+            update(1, j, m, 2);
+        (sum2 += (ll) tr[1].v * fp(2, i + 1) % mod) %= mod;
+    }
+    // cout << 
+    for (int i = 1; i <= m; i++) vl[i] = vl[i] * 3ll % mod;
+    build(1, 1, m - 1);
+    for (int i = 0; i <= (int) as.size() || i <= 10; i++) {
+        for (int j : mk[i])
+            update(1, j, m - 1, 2);
+        (sum3 += (ll) tr[1].v * fp(2, i) % mod) %= mod;
+    }
+    build(1, 1, m - 1);
+    for (int i = 0; i < (int) as.size() || i < 10; i++) {
+        for (int j : mk[i])
+            update(1, j, m - 1, 2);
+        (sum4 += (ll) tr[1].v * fp(2, i + 1) % mod) %= mod;
+    }
+    cout << sum1 << ' ' << sum2 << ' ' << sum3 << ' ' << sum4 << endl;
+    sum1 -= sum2;
+    sum1 -= sum3;
+    sum1 += sum4;
+    cout << (sum1 + mod) % mod << endl;
     return 0;
 }
