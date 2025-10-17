7919da9950483ac4044c35d62d88db0d2dd37b93
= <=>
2025-10-17 10:21:07 +0800

    .

---
+++ StarContest/2025/10/17/lcm.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+constexpr int N = 1e5 + 10;
+int n, a[N], b[N];
+int main() {
+    cin >> n;
+    for (int i = 1; i <= n; i++) cin >> a[i] >> b[i];
+    for (int i = 1; i <= n; i++) {
+        ;
+    }
+    return 0;
+}
