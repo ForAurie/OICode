536934a6ea13e7c2f768d9a0488ef8987a16bdf6
= <=>
2025-10-20 17:40:18 +0800

    .

--- Codeforces/Contest/2154/D.cpp
+++ Codeforces/Contest/2154/D.cpp
@@ -7,12 +7,13 @@ void solve() {
     cin >> n;
     vector<bool> dis(n + 1);
     vector<vector<int>> G(n + 1), leaf(2);
+    vector<int> fa(n + 1), tmp, d(n + 1);
     for (int i = 1; i < n; i++) {
         cin >> x >> y;
         G[x].push_back(y);
         G[y].push_back(x);
+        ++d[x], ++d[y];
     }
-    vector<int> fa(n + 1);
     function<void(int)> dfs = [&](int u) {
         for (int v : G[u]) {
             if (v == fa[u]) continue;
@@ -24,7 +25,6 @@ void solve() {
     dfs(1);
     int now = n;
     vector<bool> mk(n + 1), vis(n + 1);
-    vector<int> tmp;
     while (now) {
         mk[now] = 1;
         tmp.push_back(now);
@@ -33,14 +33,10 @@ void solve() {
     now = 0;
     for (int i = 1; i <= n; i++) {
         if (mk[i]) continue;
-        if (G[i].size() <= 1) leaf[dis[i]].push_back(i), vis[i] = 1;
+        if (d[i] <= 1) leaf[dis[i]].push_back(i), vis[i] = 1;
     }
     vector<int> ans;
     while (leaf[0].size() || leaf[1].size()) {
-        // for (auto i : leaf[0]) cout << ":" << i;
-        // cout << '\n';
-        // for (auto i : leaf[1]) cout << ":" << i;
-        // cout << "\n\n";
         ans.push_back(0);
         now ^= 1;
         if (leaf[0].size()) {
@@ -48,7 +44,7 @@ void solve() {
             ans.push_back(leaf[0].back());
             for (int v : G[leaf[0].back()]) {
                 if (mk[v] || vis[v]) continue;
-                leaf[dis[v]].push_back(v), vis[v] = 1;
+                if (--d[v] <= 1) leaf[dis[v]].push_back(v), vis[v] = 1;
             }
             leaf[0].pop_back();
         } else {
@@ -56,15 +52,14 @@ void solve() {
             ans.push_back(leaf[1].back());
             for (int v : G[leaf[1].back()]) {
                 if (mk[v] || vis[v]) continue;
-                leaf[dis[v]].push_back(v), vis[v] = 1;
+                if (--d[v] <= 1) leaf[dis[v]].push_back(v), vis[v] = 1;
             }
             leaf[1].pop_back();
         }
     }
-    reverse(tmp.begin(), tmp.end());
-    while (tmp.back() != 1) {
+    while (tmp.back() != n) {
         ans.push_back(0); now ^= 1;
-        if ((dis[tmp.back() & 1]) == now) ans.push_back(0), now ^= 1;
+        if ((dis[tmp.back()] & 1) == now) ans.push_back(0), now ^= 1;
         ans.push_back(tmp.back());
         tmp.pop_back();
     }
