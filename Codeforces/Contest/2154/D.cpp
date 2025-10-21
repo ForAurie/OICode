9ef4540a8c24e1c463b189bcd614c05cfdf2d482
= <=>
2025-10-21 16:23:38 +0800

    .

--- Codeforces/Contest/2154/D.cpp
+++ Codeforces/Contest/2154/D.cpp
@@ -1,79 +1,65 @@
-#include <bits/stdc++.h>
+    #include <bits/stdc++.h>
 
-using namespace std;
+    using namespace std;
 
-void solve() {
-    int n, x, y;
-    cin >> n;
-    vector<bool> dis(n + 1);
-    vector<vector<int>> G(n + 1), leaf(2);
-    vector<int> fa(n + 1), tmp, d(n + 1);
-    for (int i = 1; i < n; i++) {
-        cin >> x >> y;
-        G[x].push_back(y);
-        G[y].push_back(x);
-        ++d[x], ++d[y];
-    }
-    function<void(int)> dfs = [&](int u) {
-        for (int v : G[u]) {
-            if (v == fa[u]) continue;
-            dis[v] = dis[u] ^ 1;
-            fa[v] = u;
-            dfs(v);
+    void solve() {
+        int n, x, y;
+        cin >> n;
+        vector<bool> dis(n + 1);
+        vector<vector<int>> G(n + 1);
+        array<stack<int>, 2> leaf;
+        vector<int> d(n + 1);
+        for (int i = 1; i < n; i++) {
+            cin >> x >> y;
+            G[x].push_back(y);
+            G[y].push_back(x);
+            ++d[x], ++d[y];
         }
-    };
-    dfs(1);
-    int now = n;
-    vector<bool> mk(n + 1), vis(n + 1);
-    while (now) {
-        mk[now] = 1;
-        tmp.push_back(now);
-        now = fa[now];
-    }
-    now = 0;
-    for (int i = 1; i <= n; i++) {
-        if (mk[i]) continue;
-        if (d[i] <= 1) leaf[dis[i]].push_back(i), vis[i] = 1;
-    }
-    vector<int> ans;
-    while (leaf[0].size() || leaf[1].size()) {
-        ans.push_back(0);
-        now ^= 1;
-        if (leaf[0].size()) {
-            if (now == 0) ans.push_back(0), now ^= 1;
-            ans.push_back(leaf[0].back());
-            for (int v : G[leaf[0].back()]) {
-                if (mk[v] || vis[v]) continue;
-                if (--d[v] <= 1) leaf[dis[v]].push_back(v), vis[v] = 1;
+        function<void(int, int)> dfs = [&](int u, int fa) {
+            for (int v : G[u]) {
+                if (v == fa) continue;
+                dis[v] = dis[u] ^ 1;
+                dfs(v, u);
             }
-            leaf[0].pop_back();
-        } else {
-            if (now == 1) ans.push_back(0), now ^= 1;
-            ans.push_back(leaf[1].back());
-            for (int v : G[leaf[1].back()]) {
-                if (mk[v] || vis[v]) continue;
-                if (--d[v] <= 1) leaf[dis[v]].push_back(v), vis[v] = 1;
+        };
+        dfs(1, 1);
+        int now = 0;
+        for (int i = 1; i < n; i++)
+            if (d[i] == 1) leaf[dis[i]].push(i), d[i] = 0;
+        vector<int> ans;
+        while (leaf[0].size() || leaf[1].size()) {
+            ans.push_back(0);
+            now ^= 1;
+            if (leaf[0].size()) {
+                if (now == 0) ans.push_back(0), now ^= 1;
+                ans.push_back(leaf[0].top());
+                for (int v : G[leaf[0].top()]) {
+                    if (v == n || d[v] == 0) continue;
+                    if (--d[v] == 1) leaf[dis[v]].push(v), d[v] = 0;
+                }
+                leaf[0].pop();
+            } else {
+                if (now == 1) ans.push_back(0), now ^= 1;
+                ans.push_back(leaf[1].top());
+                for (int v : G[leaf[1].top()]) {
+                    if (v == n || d[v] == 0) continue;
+                    if (--d[v] == 1) leaf[dis[v]].push(v), d[v] = 0;
+                }
+                leaf[1].pop();
             }
-            leaf[1].pop_back();
         }
+        cout << ans.size() << '\n';
+        for (auto i : ans) {
+            if (i) cout << "2 " << i << '\n';
+            else cout << "1\n";
+        }
+        cout << '\n';
     }
-    while (tmp.back() != n) {
-        ans.push_back(0); now ^= 1;
-        if ((dis[tmp.back()] & 1) == now) ans.push_back(0), now ^= 1;
-        ans.push_back(tmp.back());
-        tmp.pop_back();
-    }
-    cout << ans.size() << '\n';
-    for (auto i : ans) {
-        if (i) cout << "2 " << i << '\n';
-        else cout << "1\n";
-    }
-    cout << '\n';
-}
 
-int main() {
-    int t;
-    cin >> t;
-    while (t--) solve();
-    return 0;
-}
+    int main() {
+        ios::sync_with_stdio(false); cin.tie(nullptr);
+        int t;
+        cin >> t;
+        while (t--) solve();
+        return 0;
+    }
