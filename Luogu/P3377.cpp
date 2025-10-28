3ac7d18c7afda6352efe58ec7ef70327254e022d
= <=>
2025-10-28 17:22:12 +0800

    .

--- Luogu/P3377.cpp
+++ Luogu/P3377.cpp
@@ -7,17 +7,14 @@ private:
     T val;
     unique_ptr<LeftistTree> ls, rs;
     size_t h;
-
 public:
     explicit LeftistTree(T val)
         : val(std::move(val)), ls(nullptr), rs(nullptr), h(1) {}
-
     LeftistTree(LeftistTree&& other) noexcept
         : val(std::move(other.val)),
           ls(std::move(other.ls)),
           rs(std::move(other.rs)),
           h(other.h) {}
-
     LeftistTree& operator=(LeftistTree&& other) noexcept {
         if (this != &other) {
             val = std::move(other.val);
@@ -27,11 +24,9 @@ public:
         }
         return *this;
     }
-
     // 禁用拷贝
     LeftistTree(const LeftistTree&) = delete;
     LeftistTree& operator=(const LeftistTree&) = delete;
-
     static unique_ptr<LeftistTree> merge(unique_ptr<LeftistTree> a, unique_ptr<LeftistTree> b) {
         if (!a) return b;
         if (!b) return a;
@@ -44,12 +39,10 @@ public:
         a->h = (a->rs ? a->rs->h : 0) + 1;
         return a;
     }
-
     static unique_ptr<LeftistTree> pop(unique_ptr<LeftistTree> root) {
         if (!root) return nullptr;
         return merge(std::move(root->ls), std::move(root->rs));
     }
-
     const T& top() const { return val; }
 };
 
